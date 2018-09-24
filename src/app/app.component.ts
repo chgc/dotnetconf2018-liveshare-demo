65d026c63843ef725594ef85ddfebc218ca32f48
Kevin Yang <chgc_kevin@hotmail.com>
2018-09-24 11:39:56 +0800

    新增加入 Todo 功能

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo';
+  newTodo = '';
+  todos = [];
+
+  addTodo(){
+    this.todos.push(this.newTodo);
+    this.newTodo = '';
+  }
 }
