6f3eef2ffaff8d0043e47ce4b15ad4c8db63d878
Kevin Yang <chgc_kevin@hotmail.com>
2018-09-24 21:45:07 +0800

    用 RxJS 改寫

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,19 @@
 import { Component } from '@angular/core';
 import { Todo } from './model/todo';
 import { ActivatedRoute } from '@angular/router';
+import { Subject, Observable } from 'rxjs';
+import { map, scan, shareReplay } from 'rxjs/operators';
+
+type ActionType =
+  | 'Add'
+  | 'Update'
+  | 'Delete'
+  | 'MarkAllComplete'
+  | 'clearComplete'
+  | 'CancelAllEdit';
+export class Action {
+  constructor(public action: ActionType, public todo?: Todo) {}
+}
 
 @Component({
   selector: 'app-root',
@@ -8,64 +21,90 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
   newTodo = '';
-  todos: Todo[] = [];
+
+  dispatch$ = new Subject<Action>();
+  todos$: Observable<Todo[]> = this.dispatch$.pipe(
+    scan((acc: Todo[], value: Action) => {
+      switch (value.action) {
+        case 'Add':
+          const maxId =
+            (acc
+              .slice()
+              .sort((a, b) => b.id - a.id)
+              .map(todo => todo.id)[0] || 0) + 1;
+          return [...acc, new Todo(maxId, value.todo.content)];
+        case 'Update':
+          return acc.map(todo => {
+            if (todo.id === value.todo.id) {
+              todo = value.todo;
+            }
+            return todo;
+          });
+        case 'Delete':
+          return acc.filter(todo => todo.id !== value.todo.id);
+        case 'CancelAllEdit':
+          return acc.map(
+            _todo => new Todo(_todo.id, _todo.content, _todo.isCompleted, false)
+          );
+        case 'clearComplete':
+          return acc.filter(todo => !todo.isCompleted);
+        case 'MarkAllComplete':
+          return acc.map(
+            todo => new Todo(todo.id, todo.content, true, todo.isEdit)
+          );
+      }
+      return acc;
+    }, []),
+    shareReplay()
+  );
   filter = '';
   constructor(private route: ActivatedRoute) {
     route.fragment.subscribe(fragment => {
       this.filter = (fragment || '/').replace('/', '');
     });
   }
-  get itemLeft() {
-    return this.todos.filter(todo => !todo.isCompleted).length;
-  }
+  itemLeft$ = this.todos$.pipe(
+    map(todos => todos.filter(todo => !todo.isCompleted).length)
+  );
+
+  todoCount$ = this.todos$.pipe(map(todos => todos.length));
 
   addTodo() {
-    this.todos.push(new Todo(this.newTodo));
+    this.dispatch$.next(new Action('Add', Todo.create(this.newTodo)));
     this.newTodo = '';
   }
 
-  toggleComplete(todo: Todo, idx) {
-    this.refreshTodoList(idx, todo.toggleComplete());
+  toggleComplete(todo: Todo) {
+    this.dispatch$.next(new Action('Update', todo.toggleComplete()));
   }
 
-  removeTodo(idx) {
-    this.refreshTodoList(idx);
+  removeTodo(todo: Todo) {
+    this.dispatch$.next(new Action('Delete', todo));
   }
 
   markAllComplete() {
-    this.todos = this.todos.map(todo => new Todo(todo.content, true));
+    this.dispatch$.next(new Action('MarkAllComplete'));
   }
 
   clearComplete() {
-    this.todos = this.todos.filter(todo => !todo.isCompleted);
+    this.dispatch$.next(new Action('clearComplete'));
   }
 
-  enterEdit(todo: Todo, idx, target: HTMLInputElement) {
+  enterEdit(todo: Todo, target: HTMLInputElement) {
     if (todo.isCompleted) {
       return;
     }
-    this.todos = this.todos.map(
-      _todo => new Todo(_todo.content, _todo.isCompleted, false)
-    );
-    this.refreshTodoList(idx, todo.toggleEdit());
+    this.dispatch$.next(new Action('CancelAllEdit'));
+    this.dispatch$.next(new Action('Update', todo.toggleEdit()));
     target.focus();
   }
 
-  updateContent(content: string, idx) {
-    this.refreshTodoList(idx, new Todo(content));
+  updateContent(content, todo) {
+    this.dispatch$.next(new Action('Update', todo.updateContent(content)));
   }
 
-  cancelUpdate(todo: Todo, idx) {
-    this.refreshTodoList(idx, todo.toggleEdit());
-  }
-
-  private refreshTodoList(idx, todo: Todo | null = null) {
-    if (!!todo) {
-      this.todos.splice(idx, 1, todo);
-    } else {
-      this.todos.splice(idx, 1);
-    }
+  cancelUpdate(todo: Todo) {
+    this.dispatch$.next(new Action('Update', todo.toggleEdit()));
   }
 }
--- src/app/model/todo.ts
+++ src/app/model/todo.ts
@@ -2,25 +2,31 @@ export class Todo {
   readonly content: string;
   readonly isCompleted: boolean;
   readonly isEdit: boolean;
+  readonly id: number;
   constructor(
+    id: number,
     content: string,
     isCompleted: boolean = false,
     isEdit: boolean = false
   ) {
+    this.id = id;
     this.content = content;
     this.isCompleted = isCompleted;
     this.isEdit = isEdit;
   }
 
+  static create(content: string) {
+    return new Todo(0, content);
+  }
   toggleComplete() {
-    return new Todo(this.content, !this.isCompleted);
+    return new Todo(this.id, this.content, !this.isCompleted);
   }
 
   updateContent(content: string) {
-    return new Todo(content, this.isCompleted);
+    return new Todo(this.id, content, this.isCompleted);
   }
 
   toggleEdit() {
-    return new Todo(this.content, this.isCompleted, !this.isEdit);
+    return new Todo(this.id, this.content, this.isCompleted, !this.isEdit);
   }
 }
